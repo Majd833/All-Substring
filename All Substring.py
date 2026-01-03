5ef26363454cb6d087fee23a2374b03dad7bf2ec
Majd833 <lina.azzam.1611@gmail.com>
2026-01-03 18:41:47 +0200

    hw

--- All Substring.py
+++ All Substring.py
@@ -1,16 +1,20 @@
-import math
-def powerset(set,size):
-    powersetsize=(int) (math.pow(2,size))
-    outer=0
-    inner=0
-    for outer in range (0,powersetsize):
-        for inner in range(0,size):
-            if ((outer&(1<<inner))>0):
-                print(set[inner],end="")
-        print("")
-size=int(input("Enter array size:"))
-set=[]
-for i in range(0,size):
-    n=int(input("Enter element:"))
-    set.append(n)
-powerset(set,len(set))
+def powerset(string_list, size):
+    powersetsize = 2**size
+
+    for outer in range(0, powersetsize):
+        subset = []
+        for inner in range(0, size):
+            if ((outer & (1 << inner)) > 0):
+                subset.append(string_list[inner])
+        
+        print(f"{{{', '.join(subset)}}}")
+
+size = int(input("Enter number of elements: "))
+elements = []
+
+for i in range(0, size):
+    item = input(f"Enter element {i+1}: ")
+    elements.append(item)
+
+print("\nAll possible subsets (Powerset):")
+powerset(elements, len(elements))
